1c2101d89df1e655ae3ab95fb495a34bc65dd016
mammadli <gulnar.mammadli@ut.ee>
2023-07-21 23:57:29 +0300

    Added Swagger customized description for APIs

--- backend/ems/src/main/java/com/mammadli/ems/controller/EmployeeController.java
+++ backend/ems/src/main/java/com/mammadli/ems/controller/EmployeeController.java
@@ -2,6 +2,12 @@ package com.mammadli.ems.controller;
 
 import com.mammadli.ems.dto.EmployeeDto;
 import com.mammadli.ems.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,32 +19,58 @@ import java.util.List;
 @CrossOrigin("*")
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
+@Tag(name = "Employee",description = "Employee management APIs")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    @Operation(summary = "Create a new employee")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Created the employee",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDto.class))})})
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@Valid @RequestBody EmployeeDto dto) {
         return ResponseEntity.ok().body(employeeService.createEmployee(dto));
     }
 
+    @Operation(summary = "Fetch employee by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the employee",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDto.class))})})
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeDto> fetchById(@PathVariable Long id) {
         return ResponseEntity.ok().body(employeeService.fetchById(id));
     }
 
+    @Operation(summary = "Fetch employee list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Fetched successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDto.class))})})
     @GetMapping()
     public ResponseEntity<List<EmployeeDto>> fetchAllEmployees() {
         return ResponseEntity.ok().body(employeeService.fetchAll());
     }
 
+    @Operation(summary = "Delete employee")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deleted the employee",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Long.class))})})
     @DeleteMapping("/{id}")
     public ResponseEntity<Long> delete(@PathVariable Long id) {
         return ResponseEntity.ok().body(employeeService.delete(id));
     }
 
+    @Operation(summary = "Partial update employee")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Partially updated the employee",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDto.class))})})
     @PatchMapping
-    public ResponseEntity<EmployeeDto> partialUpdate(@Valid @RequestBody EmployeeDto dto){
+    public ResponseEntity<EmployeeDto> partialUpdate(@Valid @RequestBody EmployeeDto dto) {
         return ResponseEntity.ok().body(employeeService.partialUpdate(dto));
     }
 }
